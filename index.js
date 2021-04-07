20b109c8c56f61c4f2495dcabbf6049106a39141
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-04-07 09:44:11 -0700

    init fxn added to start app

--- index.js
+++ index.js
@@ -5,15 +5,32 @@ const questions = [
     {
         type: 'input',
         name: 'nameInput',
-        message: 'Enter a name.'
+        message: "Enter your teammate's name."
     },
-   
+    {
+        type: 'input',
+        name: 'emailInput',
+        message: "Enter your teammate's email."
+    },
+    {
+        type: 'input',
+        name: 'officeInput',
+        message: 'Enter office number.'
+    },
+    {
+        type: 'input',
+        name: 'idInput',
+        message: 'Enter employee id.'
+    }
 ];
 
 
 const init = () => {
-    console.log('Welcome to Team Profile Generator. Please enter information for your first team member.')
-    inquirer.prompt(questions);
+    console.log('Welcome to Team Profile Generator. Please enter information for your team manager.')
+    inquirer.prompt(questions)
+        .then(answers => {
+            console.log(answers)
+        });
 };
 
 init();
