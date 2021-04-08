9b319d5a378f9a9dd39025bca7458ba01ac428a7
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-04-08 10:08:58 -0700

    correctly generates card with inputted information

--- src/card-template.js
+++ src/card-template.js
@@ -1,11 +1,11 @@
-const generateCard = () => {
+const generateCard = (name, getRole, id, email, officeNumber) => {
     return `<div class = "card">
-        <div class="bg-primary text-light card-header"><h2 class= "text-light">NAME - Role</h2></div>
+        <div class="bg-primary text-light card-header"><h2 class= "text-light">${name}- ${getRole}</h2></div>
         <div class="bg-light">
             <ul class="list-group list-group-flush">
-                <li class="list-group-item">ID: </li>
-                <li class="list-group-item">Email: </li>
-                <li class="list-group-item">Github/Office number</li>
+                <li class="list-group-item">ID: ${id} </li>
+                <li class="list-group-item">Email: ${email} </li>
+                <li class="list-group-item">Github/Office number: ${officeNumber}</li>
             </ul>
         </div>
     </div>`
--- src/page-template.js
+++ src/page-template.js
@@ -16,8 +16,8 @@ return `
     <!-- header -->
     <header class="d-flex bg-primary text-light justify-content-center"><h1>My Team</h1></header>
     <!-- main -->
-    <main class= "mt-1 container">
-        ${generateCard()}
+    <main class= "mt-5 container">
+        ${generateCard('Sydney', 'Manager', 12, 'sydney.walcoff@gmail.com', 1)}
     </main>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
