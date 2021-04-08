ebf47583edf7b7a3b566b7567303debfda176033
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-04-08 10:28:05 -0700

    wrote engineer card info - links click through

--- src/card-template.js
+++ src/card-template.js
@@ -1,14 +1,35 @@
-const generateCard = (name, getRole, id, email, officeNumber) => {
-    return `<div class = "card">
-        <div class="bg-primary text-light card-header"><h2 class= "text-light">${name}- ${getRole}</h2></div>
-        <div class="bg-light">
+const generateCard = (name, getRole, id, email, officeNumber, github) => {
+    if(getRole === 'Manager') {
+        return `<div class = "card">
+        <div class="bg-primary text-light card-header">
+            <h2 class= "text-light">${name}</h2>
+            <h2 class= "text-light">${getRole}</h2>
+        </div>
+        <div class="bg-light card-body">
             <ul class="list-group list-group-flush">
                 <li class="list-group-item">ID: ${id} </li>
-                <li class="list-group-item">Email: ${email} </li>
-                <li class="list-group-item">Github/Office number: ${officeNumber}</li>
+                <li class="list-group-item">Email: <a href="mailto: ${email}">${email}</a> </li>
+                <li class="list-group-item">Office number: ${officeNumber}</li>
             </ul>
         </div>
     </div>`
+    }
+    if(getRole ==='Engineer') {
+        return `<div class = "card">
+        <div class="bg-primary text-light card-header">
+            <h2 class= "text-light">${name}</h2>
+            <h2 class= "text-light">${getRole}</h2>
+        </div>
+        <div class="bg-light card-body">
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${id} </li>
+                <li class="list-group-item">Email: <a href="mailto: ${email}">${email}</a> </li>
+                <li class="list-group-item">Github: <a href="https://github.com/${github}">${github}</a> </li>
+            </ul>
+        </div>
+    </div>`
+    }
 }
+
 
 module.exports = generateCard;
--- src/page-template.js
+++ src/page-template.js
@@ -17,7 +17,7 @@ return `
     <header class="d-flex bg-primary text-light justify-content-center"><h1>My Team</h1></header>
     <!-- main -->
     <main class= "mt-5 container">
-        ${generateCard('Sydney', 'Manager', 12, 'sydney.walcoff@gmail.com', 1)}
+        ${generateCard('Sydney', 'Engineer', 12, 'sydney.walcoff@gmail.com', 'N/A', 'sydneywalcoff')}
     </main>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
