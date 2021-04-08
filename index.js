557c533cb829ff38368022c77888615b876f0d05
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-04-08 11:07:38 -0700

    created team array to store team members

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generatePage = require('./src/page-template');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+const team = [];
 
 const managerQuestions = [
     {
@@ -20,7 +25,35 @@ const managerQuestions = [
     },
     {
         type: 'input',
-        name: 'idInput',
+        name: 'id',
+        message: 'Enter their employee id.'
+    },
+    {
+        type: 'list',
+        name: 'confirmAddEmployee',
+        message: 'Do you want to add another team member, or finish building your team?',
+        choices: ['add engineer', 'add intern', 'finish building team']
+    }
+];
+const engineerQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: "Enter their name."
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "Enter their email address."
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter their github username.'
+    },
+    {
+        type: 'input',
+        name: 'id',
         message: 'Enter their employee id.'
     },
     {
@@ -31,17 +64,26 @@ const managerQuestions = [
     }
 ];
 
+// const 
+
 
 const init = () => {
     console.log("Welcome to Team Profile Generator. Let's start with the team manager.")
     inquirer.prompt(managerQuestions)
         .then(answers => {
-            if(answers.confirmAddEmployee === 'add engineer') {
-                console.log("you've added an engineer")
-            }
-            else if (answers.confirmAddEmployee === 'add intern') {
-                console.log("you've added an intern")
-            }
+            const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+            team.push(manager);
+
+            // if(answers.confirmAddEmployee === 'add engineer') {
+            //     console.log("you're adding an engineer");
+            //     inquirer.prompt(engineerQuestions)
+            //         .then(answers => {
+            //             const engineer
+            //         });
+            // }
+        //     else if (answers.confirmAddEmployee === 'add intern') {
+        //         console.log("you've added an intern")
+        //     }
         });
 };
 
