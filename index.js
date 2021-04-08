f3dda19811ed9398f1f67d7dec9a835d479b660b
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-04-08 16:29:20 -0700

    just kidding put the role questions back in the global scope

--- index.js
+++ index.js
@@ -5,68 +5,158 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
-const promptManager = () => {
-    const managerQuestions = [
-        {
-            type: 'input',
-            name: 'name',
-            message: "Enter their name.",
-            validate: name => {
-                if(name) {
-                    return true;
-                } else {
-                    console.log("Please enter your team manager's name.");
-                    return false;
-                }
+const managerQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: "Enter their name.",
+        validate: name => {
+            if(name) {
+                return true;
+            } else {
+                console.log("Please enter your team manager's name.");
+                return false;
             }
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: "Enter their email address.",
-            validate: email => {
-                if(email) {
-                    return true;
-                } else {
-                    console.log("Please enter your team manager's email.");
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'officeNumber',
-            message: 'Enter their office number.',
-            validate: officeNumber => {
-                if(officeNumber) {
-                    return true;
-                } else {
-                    console.log("Please enter your team manager's office number.");
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'id',
-            message: 'Enter their employee id.',
-            validate: id => {
-                if(id) {
-                    return true;
-                } else {
-                    console.log("Please enter your team manager's id.");
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'list',
-            name: 'confirmAddEmployee',
-            message: 'Do you want to add another team member, or finish building your team?',
-            choices: ['add engineer', 'add intern', 'finish building team']
         }
-    ];
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "Enter their email address.",
+        validate: email => {
+            if(email) {
+                return true;
+            } else {
+                console.log("Please enter your team manager's email.");
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: 'Enter their office number.',
+        validate: officeNumber => {
+            if(officeNumber) {
+                return true;
+            } else {
+                console.log("Please enter your team manager's office number.");
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'Enter their employee id.',
+        validate: id => {
+            if(id) {
+                return true;
+            } else {
+                console.log("Please enter your team manager's id.");
+                return false;
+            }
+        }
+    },
+    {
+        type: 'list',
+        name: 'confirmAddEmployee',
+        message: 'Do you want to add another team member, or finish building your team?',
+        choices: ['add engineer', 'add intern', 'finish building team']
+    }
+];
 
+const engineerQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: "Enter their name.",
+        validate: name => {
+            if(name) {
+                return true;
+            } else {
+                console.log("Please enter their name.");
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "Enter their email address.",
+        validate: email => {
+            if(email) {
+                return true;
+            } else {
+                console.log("Please enter their email.");
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter their github username.',
+        validate: github => {
+            if(github) {
+                return true;
+            } else {
+                console.log("Please enter their github username.");
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'Enter their employee id.',
+        validate: id => {
+            if(id) {
+                return true;
+            } else {
+                console.log("Please enter their employee id.");
+                return false;
+            }
+        }
+    },
+    {
+        type: 'list',
+        name: 'confirmAddEmployee',
+        message: 'Do you want to add another team member, or finish building your team?',
+        choices: ['add engineer', 'add intern', 'finish building team']
+    }
+];
+
+const internQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: "Enter their name."
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "Enter their email address."
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: 'Enter their school.'
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'Enter their employee id.'
+    },
+    {
+        type: 'list',
+        name: 'confirmAddEmployee',
+        message: 'Do you want to add another team member, or finish building your team?',
+        choices: ['add engineer', 'add intern', 'finish building team']
+    }
+];
+
+const promptManager = (managerQuestions) => {
     inquirer.prompt(managerQuestions)
     .then(answers => {
         const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
@@ -82,72 +172,13 @@ const promptManager = () => {
             promptIntern(internQuestions, manager);
         }
         else {
-            return;
+            createDistFiles();
         }
     });
 };
 
 const promptEngineer = manager => {
-    const engineerQuestions = [
-        {
-            type: 'input',
-            name: 'name',
-            message: "Enter their name.",
-            validate: name => {
-                if(name) {
-                    return true;
-                } else {
-                    console.log("Please enter their name.");
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: "Enter their email address.",
-            validate: email => {
-                if(email) {
-                    return true;
-                } else {
-                    console.log("Please enter their email.");
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'github',
-            message: 'Enter their github username.',
-            validate: github => {
-                if(github) {
-                    return true;
-                } else {
-                    console.log("Please enter their github username.");
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'id',
-            message: 'Enter their employee id.',
-            validate: id => {
-                if(id) {
-                    return true;
-                } else {
-                    console.log("Please enter their employee id.");
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'list',
-            name: 'confirmAddEmployee',
-            message: 'Do you want to add another team member, or finish building your team?',
-            choices: ['add engineer', 'add intern', 'finish building team']
-        }
-    ];
+
     inquirer.prompt(engineerQuestions)
     .then(answers => {
         const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
@@ -169,34 +200,6 @@ const promptEngineer = manager => {
 };
 
 const promptIntern = manager => {
-    const internQuestions = [
-        {
-            type: 'input',
-            name: 'name',
-            message: "Enter their name."
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: "Enter their email address."
-        },
-        {
-            type: 'input',
-            name: 'school',
-            message: 'Enter their school.'
-        },
-        {
-            type: 'input',
-            name: 'id',
-            message: 'Enter their employee id.'
-        },
-        {
-            type: 'list',
-            name: 'confirmAddEmployee',
-            message: 'Do you want to add another team member, or finish building your team?',
-            choices: ['add engineer', 'add intern', 'finish building team']
-        }
-    ];
     inquirer.prompt(internQuestions)
     .then(answers => {
         const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
@@ -214,11 +217,19 @@ const promptIntern = manager => {
     });
 };
 
+const createDistFiles = manager => {
+    fs.writeFile('./dist/index.html', generatePage(manager), err => {
+        if (err) throw err;
+
+        fs.copyFile('./src/style.css', './dist/style.css', err => {
+            if(err) throw err;
+        });
+    });
+};
 
 const init = () => {
     console.log("Welcome to Team Profile Generator. Let's start with the team manager.")
     promptManager(managerQuestions);
 };
-
 
 init();
