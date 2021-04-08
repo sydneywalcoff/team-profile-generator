e30d7bed070c92a6475941bd88824fe5e3fe7d55
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-04-07 20:52:48 -0700

    broke questions up into different sections to use conditionally

--- index.js
+++ index.js
@@ -2,56 +2,81 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const generatePage = require('./src/page-template');
 
-const questions = [
+const employeeQuestions = [
     {
         type: 'input',
-        name: 'nameInput',
-        message: "Enter the team manager's name."
+        name: 'name',
+        message: "Enter  name."
     },
     {
         type: 'input',
-        name: 'emailInput',
+        name: 'email',
         message: "Enter the manager's email address."
     },
     {
-        type: 'input',
-        name: 'officeInput',
+        type: 'number',
+        name: 'officeNumber',
         message: 'Enter office number.'
     },
     {
-        type: 'input',
-        name: 'idInput',
+        type: 'number',
+        name: 'id',
         message: 'Enter employee id.'
     },
     {
         type: 'list',
-        name: 'confirmAddEmployee',
-        message: 'Do you want to add an engineer or intern, or finish building your team?',
+        name: 'addEmployee',
+        message: 'Do you want to add an engineer, or an intern, or finish building your team?',
         choices: ['add engineer', 'add intern', 'finish building team']
     },
     {
         type: 'input',
-        name: 'idInput',
-        message: 'Enter employee id.',
-        when: ({confirmAddEmployee})
-    },
-    
-    
+        name: 'name',
+        message: 'Enter engineer name.',
+        when: answers => {
+            if(answers.addEmployee) {
+                return true;
+            }
+        }
+    } 
 ];
 
+const engineerQuestions = [
+    {
+        type: 'input',
+        name: 'engineerName',
+        message: "What is the engineer's name?"
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: "What is the engineer's github?"
+    }
+];
 
-// const init = () => {
-//     console.log('Welcome to Team Profile Generator. Please enter information for your team manager.')
-//     inquirer.prompt(questions)
-//         .then(answers => {
-//             console.log(answers)
-//         });
-// };
+// const 
 
-fs.writeFile('./dist/index.html', generatePage(), err => {
-    if (err) throw err;
 
-    console.log('Portfolio complete!');
-});
+const init = () => {
+    console.log('Welcome to Team Profile Generator. Please enter information for your team manager.')
+    inquirer.prompt(employeeQuestions).then(answers => {
+        if(answers.addEmployee === 'add engineer') {
+            inquirer.prompt(engineerQuestions).then(engineerAnswers => {
+                console.log(engineerAnswers)
+                // create EngineerDiv w/ engineer Answers
+                // new Engineer(employeeQuestions ... engineerQuestions)
+            })
+        }
+        if(answers.addEmployee === 'add intern') {
+            // inquirer.prompt(internQuestions).then(internAnswers => {})
+        }
+    });
+};
 
-// init();
+// fs.writeFile('./dist/index.html', generatePage(), err => {
+//     if (err) throw err;
+
+//     console.log('Portfolio complete! Check out /dist/index.html to see your generated Team Profile!');
+// });
+
+init();
