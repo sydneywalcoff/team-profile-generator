cc3972c72c8dd48d2d290a122c979aff529588f0
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-04-09 10:50:01 -0700

    cards render correctly and styling is ok

--- src/card-template.js
+++ src/card-template.js
@@ -1,33 +1,37 @@
 const generateCard = employee => {
     if(employee.getRole() ==='Engineer') {
-        return `<div class = "card">
-        <div class="bg-primary text-light card-header">
-            <h2 class= "text-light">${employee.name}</h2>
-            <h2 class= "text-light">${employee.getRole()}</h2>
+        return `
+        <div class = "card col-3 m-1">
+            <div class="bg-primary text-light card-header">
+                <h2 class= "text-light">${employee.name}</h2>
+                <h2 class= "text-light">${employee.getRole()}</h2>
+            </div>
+            <div class="bg-light card-body">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">ID: ${employee.getId()} </li>
+                    <li class="list-group-item">Email: <a href="mailto: ${employee.getEmail()}">${employee.getEmail()}</a> </li>
+                    <li class="list-group-item">Github: <a href="https://github.com/${employee.getGithub()}" target="_blank">${employee.getGithub()}</a> </li>
+                </ul>
+            </div>
         </div>
-        <div class="bg-light card-body">
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">ID: ${employee.getId()} </li>
-                <li class="list-group-item">Email: <a href="mailto: ${employee.getEmail()}">${employee.getEmail()}</a> </li>
-                <li class="list-group-item">Github: <a href="https://github.com/${employee.getGithub()}" target="_blank">${employee.getGithub()}</a> </li>
-            </ul>
-        </div>
-    </div>`
+        `;
     }
     if(employee.getRole() ==='Intern') {
-        return `<div class = "card">
-        <div class="bg-primary text-light card-header">
-            <h2 class= "text-light">${employee.name}</h2>
-            <h2 class= "text-light">${employee.getRole()}</h2>
+        return `
+        <div class = "card col-3 m-1">
+            <div class="bg-primary text-light card-header">
+                <h2 class= "text-light">${employee.name}</h2>
+                <h2 class= "text-light">${employee.getRole()}</h2>
+            </div>
+            <div class="bg-light card-body">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">ID: ${employee.getId()} </li>
+                    <li class="list-group-item">Email: <a href="mailto: ${employee.getEmail()}">${employee.getEmail()}</a> </li>
+                    <li class="list-group-item">School: ${employee.getSchool()}</li>
+                </ul>
+            </div>
         </div>
-        <div class="bg-light card-body">
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">ID: ${employee.getId()} </li>
-                <li class="list-group-item">Email: <a href="mailto: ${employee.getEmail()}">${employee.getEmail()}</a> </li>
-                <li class="list-group-item">School: ${employee.getSchool()}</li>
-            </ul>
-        </div>
-    </div>`
+        `;
     }
 }
 
